d008d942cfd0852ea1df32218278c165b6d41da6
masx200 <masx200@qq.com>
2026-03-02 17:52:18 +0800

    feat: 在测试脚本中添加ipv6.ping0.cc/geo请求

--- test.ps1
+++ test.ps1
@@ -4,7 +4,7 @@ for (; ; ) {
 
     start-sleep 10
 
-    curl -x socks5://127.0.0.1:1080 https://api-ipv6.ip.sb/cdn-cgi/trace  -v  --doh-url https://pngwczx94z.cloudflare-gateway.com/dns-query -H "user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36" -U biiacuuruavimks9d4cn:biiacuuruavimks9d4cn https://www.cloudflare.com/cdn-cgi/trace  
+    curl -x socks5://127.0.0.1:1080 https://api-ipv6.ip.sb/cdn-cgi/trace  -v  --doh-url https://pngwczx94z.cloudflare-gateway.com/dns-query -H "user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36" -U biiacuuruavimks9d4cn:biiacuuruavimks9d4cn https://www.cloudflare.com/cdn-cgi/trace  https://ipv6.ping0.cc/geo
 
 
     start-sleep 10
