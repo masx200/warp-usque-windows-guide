5d42dabbf54b8b2ddfed2e7e09953db848cc4959
masx200 <masx200@qq.com>
2026-03-09 16:54:23 +0800

    chore: 添加 Windows 服务启停脚本

---
+++ start.bat
@@ -0,0 +1,12 @@
+nssm start usque-3
+
+ nssm start usque-2
+
+
+ nssm start usque-1
+
+
+ nssm start usque
+
+
+ nssm start warp-monitor
---
+++ stop.bat
@@ -0,0 +1,12 @@
+nssm stop usque-3
+
+ nssm stop usque-2
+
+
+ nssm stop usque-1
+
+
+ nssm stop usque
+
+
+ nssm stop warp-monitor
